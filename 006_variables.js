da5a2f78c598f5cfd6b3e5c8a548dc69b83490f9
Samuel Paul <spaul1522@gmail.com>
2018-01-29 14:37:38 -0700

    learning the difference between var, let, and const

---
+++ 006_variables.js
@@ -0,0 +1,15 @@
+// var fullName : string = "Samuel Paul";
+// let paidAccount : boolean = true;
+// const versionNumber : number = 1.3;
+// fullName = "Sam Paul";
+// paidAccount = false;
+// versionNumber = 1.4;
+// console.log(fullName);
+// console.log(paidAccount);
+// console.log(versionNumber);
+function printName(f, l) {
+    var greeting = "Hi there, ";
+    console.log(greeting + f + " " + l);
+}
+printName("Sam", "Paul");
+//# sourceMappingURL=006_variables.js.map
---
+++ 006_variables.ts
@@ -0,0 +1,18 @@
+// var fullName : string = "Samuel Paul";
+// let paidAccount : boolean = true;
+// const versionNumber : number = 1.3;
+
+// fullName = "Sam Paul";
+// paidAccount = false;
+// versionNumber = 1.4;
+
+// console.log(fullName);
+// console.log(paidAccount);
+// console.log(versionNumber);
+
+function printName(f, l) {
+  var greeting : string = "Hi there, ";
+  console.log(greeting + f + " " + l);
+}
+
+printName ("Sam", "Paul");
